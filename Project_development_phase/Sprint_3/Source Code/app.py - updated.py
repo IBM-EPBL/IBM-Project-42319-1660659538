5e81e17bb70a100d02d2cdba60c6fc4e2872c771
Aishvarya C <71811151+aishvarya-111@users.noreply.github.com>
2022-11-18 13:07:35 +0530

    Sprint 3 uploaded

---
+++ Project_development_phase/Sprint_3/Source Code/app.py - updated.py
@@ -0,0 +1,66 @@
+from app import app
+from flask import Flask, redirect, render_template, request, session, url_for
+import ibm_db
+from .request import businessArticles, entArticles, get_news_source, healthArticles, publishedArticles, randomArticles, scienceArticles, sportArticles, techArticles, topHeadlines
+
+conn=ibm_db.connect("DATABASE=bludb; HOSTNAME=824dfd4d-99de-440d-9991-629c01b3832d.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=30119;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=prg16100;PWD=3Tdg3EFQTg7f08bD",' ',' ')
+
+@app.route('/')
+def home():
+    articles = publishedArticles()
+
+    return  render_template('home.html', articles = articles,x='login')
+
+@app.route('/headlines')
+def headlines():
+    headlines = topHeadlines()
+
+    return  render_template('headlines.html', headlines = headlines)
+
+@app.route('/articles')
+def articles():
+    random = randomArticles()
+
+    return  render_template('articles.html', random = random)
+
+@app.route('/sources')
+def sources():
+    newsSource = get_news_source()
+
+    return  render_template('sources.html', newsSource = newsSource)
+
+@app.route('/category/business')
+def business():
+    sources = businessArticles()
+
+    return  render_template('business.html', sources = sources)
+
+@app.route('/category/tech')
+def tech():
+    sources = techArticles()
+
+    return  render_template('tech.html', sources = sources)
+
+@app.route('/category/entertainment')
+def entertainment():
+    sources = entArticles()
+
+    return  render_template('entertainment.html', sources = sources)
+
+@app.route('/category/science')
+def science():
+    sources = scienceArticles()
+
+    return  render_template('science.html', sources = sources)
+
+@app.route('/category/sports')
+def sports():
+    sources = sportArticles()
+
+    return  render_template('sport.html', sources = sources)
+
+@app.route('/category/health')
+def health():
+    sources = healthArticles()
+
+    return  render_template('health.html', sources = sources)
